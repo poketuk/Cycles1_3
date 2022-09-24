8df1bb4ce0bdbd20ce2ecf46057614474779bd1f
poketuk <poketuk@yandex.ru>
2022-09-24 16:14:33 +0300

    Сделано Домашнее задание 3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Задание 1
+        System.out.println("Задание 1");
+        int part = 29000;
+        int total = 0;
+        for (int i = 1; i <= 12; i++) {
+            total += part;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей");
+        }
+
+        // Задание 2
+        System.out.println("Задание 2");
+        float totalWithPersent = 0F;
+        float percent = 0.01F;
+        for (int i = 1; i <= 12; i++) {
+            totalWithPersent = (totalWithPersent + part) * (1 + percent);
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + " %.2f " + totalWithPersent + " рублей");
+        }
     }
 }
